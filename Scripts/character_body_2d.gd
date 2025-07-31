1f697c640d1d686a98c005f3d8d4b119f66e04ee
Kekkles1 <fatima.mah24314@gmail.com>
2025-08-01 00:51:17 +0500

    added jump buffer, coyote time (not finalized

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -7,40 +7,54 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const WALL_SPEED = 0
 const WALL_JUMP_FORCE = Vector2(250, -400)
+const JUMP_BUFFER = 0.2
+const COYOTE_TIMER = 0.1
 
 var wall_dir := 0  # -1 = left wall, 1 = right wall, 0 = none
+var jump_buffer_timer := 0.0
+var coyote_timer := 0.0
 
 func _ready() -> void:
-	#Activates the camera so it follows player
+	#makes camera current and follows player
 	cam.make_current()
 
 func _physics_process(delta: float) -> void:
-	# can replace this with is_on_floor_only() if we don't wanna detect slopes im not sure rn
-	var on_floor=is_on_floor()
-	var on_wall=is_on_wall()
-	
-	#detect what a wall is
+	var on_floor = is_on_floor()
+	var on_wall = is_on_wall()
+
+	if on_floor:
+		coyote_timer = COYOTE_TIMER
+
+	# detect wall
 	if on_wall and not on_floor:
-		var wall_normal=get_wall_normal()
-		wall_dir=int(wall_normal.x)
+		var wall_normal = get_wall_normal()
+		wall_dir = int(wall_normal.x)
 	else:
-		wall_dir=0
+		wall_dir = 0
 
-	if not is_on_floor():
-		velocity+= get_gravity() * delta
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
+	if coyote_timer > 0:
+		coyote_timer -= delta
 
-#wall hang
-	if wall_dir!=0:
-		velocity.y=WALL_SPEED
-		
+	# applies gravity
+	if not on_floor:
+		velocity += get_gravity() * delta
+
 	if Input.is_action_just_pressed("ui_accept"):
-		if is_on_floor():
+		jump_buffer_timer = JUMP_BUFFER
+
+	if jump_buffer_timer > 0:
+		if on_floor or coyote_timer > 0:
 			velocity.y = JUMP_VELOCITY
+			jump_buffer_timer = 0
+			coyote_timer = 0
 		elif wall_dir != 0:
-		#Wall jump: opposite direction of wall
 			velocity.x = WALL_JUMP_FORCE.x * -wall_dir
 			velocity.y = WALL_JUMP_FORCE.y
 			sprite.play("wall-jump")
+			jump_buffer_timer = 0
+
 
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -51,10 +65,10 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 	# Animation
-	if not is_on_floor():
+	if not on_floor:
 		sprite.play("jump")
 	elif velocity.x == 0:
-		sprite.play("default") 
+		sprite.play("default")
 	else:
 		sprite.play("run")
 		sprite.flip_h = velocity.x < 0
